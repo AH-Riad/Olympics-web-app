a074dc9ad0a75b423bc11f208ecb58b3b5cb066c
Riad <riadk910@gmail.com>
2026-03-01 22:53:05 +0600

    Added sidebar

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Testing...")
+import streamlit as st
+import pandas as pd
+
+st.sidebar.radio(
+    "Select an Option",
+    ("Medal Tally", "Overall Analysis", "Country-wise-Analysis", "Athlete wise Analysis")
+)
