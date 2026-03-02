308aa0c4fe762a0cc3c1da469d16deda1c2439d9
Riad <riadk910@gmail.com>
2026-03-02 14:18:34 +0600

    Issue resolved

--- preprocessor.py
+++ preprocessor.py
@@ -1,12 +1,30 @@
 import pandas as pd
-df = df.read_csv("athlete_events.csv")
-region_df = df.read_csv("noc_regions.csv")
 
-def preprocessor(df):
+try:
+    df = pd.read_csv("athlete_events.csv")
+except pd.errors.EmptyDataError:
+    df = pd.DataFrame()
+
+try:
+    region_df = pd.read_csv("noc_regions.csv")
+except pd.errors.EmptyDataError:
+    region_df = pd.DataFrame()
+
+
+def preprocess():
     global df, region_df
-    df = df[df["Season"] == "Summer"]
-    df = df.merge(region_df, how="left", on="NOC")
-    df.drop_duplicates(inplace=True)
-    df = df.concate([df, pd.get_dummies(df["Medal"])] , axis = 1)
 
-    return df
+    if df.empty or region_df.empty:
+        return pd.DataFrame()
+
+    df_filtered = df[df["Season"] == "Summer"]
+
+    df_merged = df_filtered.merge(region_df, how="left", on="NOC")
+
+    df_merged.drop_duplicates(inplace=True)
+
+    medal_dummies = pd.get_dummies(df_merged["Medal"])
+
+    df_final = pd.concat([df_merged, medal_dummies], axis=1)
+
+    return df_final
