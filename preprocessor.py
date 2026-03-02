7dc4e4aa3c912355c6cc86b31c9f1de957f61f3e
Riad <riadk910@gmail.com>
2026-03-02 14:06:11 +0600

    Preprocessing data

--- preprocessor.py
+++ preprocessor.py
@@ -1,3 +1,12 @@
 import pandas as pd
 df = df.read_csv("athlete_events.csv")
 region_df = df.read_csv("noc_regions.csv")
+
+def preprocessor(df):
+    global df, region_df
+    df = df[df["Season"] == "Summer"]
+    df = df.merge(region_df, how="left", on="NOC")
+    df.drop_duplicates(inplace=True)
+    df = df.concate([df, pd.get_dummies(df["Medal"])] , axis = 1)
+
+    return df
