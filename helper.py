ce1d7c841480e995fe261b6189258e15117d566b
Riad <riadk910@gmail.com>
2026-03-03 13:03:35 +0600

    unique years&country

--- helper.py
+++ helper.py
@@ -5,4 +5,20 @@ def medal_tally(df):
 
     medal_tally['total'] = medal_tally['Gold'] + medal_tally['Silver'] + medal_tally['Bronze']
 
+    medal_tally['Gold'] = medal_tally['Gold'].astype(int)
+    medal_tally['Silver'] = medal_tally['Silver'].astype(int)
+    medal_tally['Bronze'] = medal_tally['Bronze'].astype(int)
+    medal_tally['total'] = medal_tally['total'].astype(int)
+
     return medal_tally
+
+def count_year_list(df):
+    years = df['Year'].unique().tolist()
+    years.sort()
+    years.insert(0, 'Overall')
+
+    country = np.unique(df['region'].dropna().values).tolist()
+    country.sort()
+    country.insert(0, 'Overall')
+
+    return years, country
