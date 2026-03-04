edeb3c0e58a00b12cf6b813cd9065bafed154437
Riad <riadk910@gmail.com>
2026-03-04 21:56:35 +0600

    Filtering Function applied

--- app.py
+++ app.py
@@ -12,12 +12,16 @@ user_menue = st.sidebar.radio(
     ("Medal Tally", "Overall Analysis", "Country-wise-Analysis", "Athlete wise Analysis")
 )
 
-st.dataframe(df.head())
-
 if user_menue == "Medal Tally":
     st.sidebar.header("Medal Tally")
     years, country = helper.country_year_list(df)
-    selected_years = st.sidebar.selectbox("Select Year", years)
+
+    selected_year = st.sidebar.selectbox("Select Year", years)
     selected_country = st.sidebar.selectbox("Select Country", country)
-    medal_tally = helper.medal_tally(df)
-    st.dataframe(medal_tally)
+
+    medal_tally = helper.fetch_medal_tally(df, selected_year, selected_country)
+    if selected_year == "Overall" and selected_country == "Overall":
+        st.title("Overall Tally")
+    if selected_year == "Overall" and selected_country == "Overall":
+
+        st.dataframe(medal_tally)
