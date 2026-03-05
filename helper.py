fc537915be57456dd3dfadd01d74b613570eac18
Riad <riadk910@gmail.com>
2026-03-05 12:43:40 +0600

    Created nations_over_time function

--- helper.py
+++ helper.py
@@ -61,3 +61,8 @@ def country_year_list(df):
     country.insert(0, 'Overall')
 
     return years, country
+
+def participating_nations_over_time(df):
+    nations_over_time = df.drop_duplicates(["Year", "region"])['Year'].value_counts().reset_index().sort_values('Year')
+
+    return nations_over_time
